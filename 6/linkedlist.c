801a4827c426dcf6232ff583649cc74e6f9fbeec
Arch me <kishudwivedi30@gmail.com>
2025-07-02 12:48:33 +0530

    linkedlist

--- 6/linkedlist.c
+++ 6/linkedlist.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct node_t {
+  int value;
+  struct node_t *next;
+  struct node_t *prev;
+} node_t;
+
+node_t *create_node (int val) {
+  
+}
