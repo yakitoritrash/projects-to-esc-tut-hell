60c4fdacbc1e9a51fbb966be6a0c1bb0c8fb8e51
Arch me <kishudwivedi30@gmail.com>
2025-07-02 15:00:40 +0530

    link

--- 6/linkedlist.c
+++ 6/linkedlist.c
@@ -32,15 +32,47 @@ node_t *prepend(node_t *head, int val) {
   return new_node;
 }
 
+node_t *append(node_t *head, int val) {
+  node_t *new_node = create_node(val);
+  if (new_node == NULL) {
+    return head;
+  }
+  if (head == NULL) {
+    return new_node;
+  }
+  node_t *tmp = head;
+  while (tmp->next != NULL) {
+    tmp = tmp->next;
+  }
+  tmp->next = new_node;
+  if (tmp != NULL) {
+    new_node->prev = tmp;
+  }
+  return head;
+}
+
+node_t *insert_at(node_t *head, int position, int val) {
+  node_t *new_node = create_node(val);
+  if (new_node == NULL) {
+    return head;
+  }
+  if (head == NULL) {
+    return NULL;
+  }
+
+  return head;
+}
+
 int main() {
   node_t *head = NULL;
   head = create_node(5);
   head = prepend(head, 10);
   head = prepend(head, 15);
+  head = append(head, 2);
 
   node_t *tmp = head;
   while (tmp != NULL) {
-    printf("%d -> ", tmp->value);
+    printf("%d <-> ", tmp->value);
     tmp = tmp->next;
   }
   printf("NULL");
