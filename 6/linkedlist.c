9c40db4b6cae19167987ad5316f7d49f3a051254
Arch me <kishudwivedi30@gmail.com>
2025-07-02 13:52:59 +0530

    link

--- 6/linkedlist.c
+++ 6/linkedlist.c
@@ -18,3 +18,29 @@ node_t *create_node (int val) {
   new_node->prev = NULL;
   return new_node;
 }
+
+node_t *prepend(node_t *head, int val) {
+  node_t *new_node = create_node(val);
+  if (new_node == NULL) {
+    return head;
+  }
+  new_node->next = head;
+  if (new_node->next) {
+    new_node->next->prev = new_node;
+  }
+  head = new_node;
+  return head;
+}
+
+int main() {
+  node_t *head = NULL;
+  create_node(5);
+  prepend(head, 10);
+  prepend(head, 15);
+
+  node_t *tmp = head;
+  while (tmp != NULL) {
+    printf("%d -> ", tmp->value);
+    tmp = tmp->next;
+  }
+}
