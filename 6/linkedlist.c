b28c7ea32069c8cf7aeb96a02c1e2fb3b260c45d
Arch me <kishudwivedi30@gmail.com>
2025-07-02 13:20:01 +0530

    linked list

--- 6/linkedlist.c
+++ 6/linkedlist.c
@@ -8,5 +8,13 @@ typedef struct node_t {
 } node_t;
 
 node_t *create_node (int val) {
-  
+  node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    printf("Memory allocation failed.");
+    return NULL;
+  }
+  new_node->value = val;
+  new_node->next= NULL;
+  new_node->prev = NULL;
+  return new_node;
 }
