73a98a117b1d92adb124259696865ed6c75c0191
Arch me <kishudwivedi30@gmail.com>
2025-07-04 14:24:20 +0530

    swap ints

--- 7/swapint.c
+++ 7/swapint.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-void swapInt(int *n1, int *n2) {
+void swap_int(int *n1, int *n2) {
   int tmp = *n1;
   *n1 = *n2;
   *n2 = tmp;
 }
 
 int main() {
-  int *n1;
-  scanf("%d", &*n1);
-  int *n2;
-  scanf("%d", &*n2);
-  printf("%d %d", *n1, *n2);
-  swapInt(n1, n2);
-  printf("%d %d", *n1, *n2);
+  int n1;
+  scanf("%d\n", &n1);
+  int n2;
+  scanf("%d", &n2);
+  printf("%d %d \n", n1, n2);
+  swap_int(&n1, &n2);
+  printf("%d %d", n1, n2);
+  return 0;
 }
