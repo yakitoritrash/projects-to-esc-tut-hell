11e53ea3182afe4a29ed62f6034bfbc579caff81
Arch me <kishudwivedi30@gmail.com>
2025-07-08 01:09:15 +0530

    with maps ?

---
+++ 9/rem-dupes-without-set.js
@@ -0,0 +1,10 @@
+function remDupes (arr) {
+  const map = new Map();
+  for (let a of arr) {
+    map.set(a, (map.get(a) || 0) + 1);
+  }
+  console.log(arr);
+  console.log(map);
+}
+
+remDupes([3, 4, 5, 6, 7, 3, 1, 1]);
