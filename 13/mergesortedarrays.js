1173b65b567d3865d5fc31adb33b5b6d6e9347c1
Arch me <kishudwivedi30@gmail.com>
2025-07-09 00:35:01 +0530

    merge two sorted arrays

--- 13/mergesortedarrays.js
+++ 13/mergesortedarrays.js
@@ -1,24 +1,30 @@
 function mergeSorted(arr1, arr2) {
   arr3 = [];
   if (arr1.length > arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
+    for (let i = 0; i < arr1.length - 1; i++) {
       if (arr1[i] <= arr2[i]) {
         arr3.push(arr1[i]);
+        arr3.push(arr2[i]);
       } else {
         arr3.push(arr2[i]);
+        arr3.push(arr1[i]);
       }
     }
   }
   if (arr2.length >= arr1.length) {
-    for (let i = 0; i < arr2.length; i++) {
+    for (let i = 0; i < arr2.length - 1; i++) {
       if (arr1[i] <= arr2[i]) {
         arr3.push(arr1[i]);
+        arr3.push(arr2[i]);
       } else {
         arr3.push(arr2[i]);
+        arr3.push(arr1[i]);
       }
     }
   }
+  console.log(arr1);
+  console.log(arr2);
   console.log(arr3);
 }
 
-mergeSorted([2, 3, 4, 5, 7], [6, 8, 9, 10]);
+mergeSorted([2, 4, 6, 7], [3, 5, 8, 9, 10]);
