4aedbe01fe72414d8dde13f439f46a28eb2b770f
Arch me <kishudwivedi30@gmail.com>
2025-07-08 11:44:06 +0530

    largest and smallest in an array

---
+++ 12/largest_smallest.js
@@ -0,0 +1,10 @@
+function largestSmallest(arr) {
+  res = [];
+  res.push(Math.max(arr));
+  res.push(Math.min(arr));
+  console.log(arr);
+  console.log(res);
+  return res;
+}
+
+largestSmallest([12, 34, 56, 32, 89, 53]);
