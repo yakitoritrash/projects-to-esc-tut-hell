2812a7ef4e3b19e0d0e997b5de842fc77e73201c
Arch me <kishudwivedi30@gmail.com>
2025-07-10 14:42:00 +0530

    tic tac toe

--- 14/texttictactoe.js
+++ 14/texttictactoe.js
@@ -1,10 +1,23 @@
 function tictactoe() {
-  const readline = require("realine");
+  const readline = require("readline");
 
+  let board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
+
+  let player =  'X';
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   })
 
-
+  function printBoard() {
+    console.log(`${board[0][0]} ${board[0][1]} ${board[0][2]}`)
+  }
+  
+  printBoard();
 } 
+
+tictactoe()
