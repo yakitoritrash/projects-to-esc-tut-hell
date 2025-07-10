0164a3c68d2a8982bf46cb2c19970fa1d06fe4a3
Arch me <kishudwivedi30@gmail.com>
2025-07-10 16:03:29 +0530

    winner function

--- 14/texttictactoe.js
+++ 14/texttictactoe.js
@@ -15,13 +15,32 @@ function tictactoe() {
 
   function printBoard() {
     console.log(`${board[0][0]} | ${board[0][1]} | ${board[0][2]}`)
-    console.log('-----------');
+    console.log('---------');
     console.log(`${board[0][0]} | ${board[0][1]} | ${board[0][2]}`)
-    console.log('-----------');
+    console.log('---------');
     console.log(`${board[0][0]} | ${board[0][1]} | ${board[0][2]}`)
     console.log("\n");
   }
-  
+
+  function checkWinner() {
+    for (let i = 0; i < 3; i++) {
+      if (board[i][1] != ' ' && board[i][1] === board[i][1] && board[i][1] === board[i][2]) {
+        return board[i][0];
+      }
+    }
+    for (let i = 0; i < 3; i++) {
+      if (board[0][i] != ' ' && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+        return board[0][i];
+      }
+    }
+    if (board[0][0] !== 0 && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+      return board[0][0];
+    }
+    if (board[0][2] !== 0 && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+      return board[0][2];
+    }
+  }
+
   printBoard();
 } 
 
