c7ea70c0e2a87a535ee735e96819104652e9ea42
Arch me <kishudwivedi30@gmail.com>
2025-07-17 14:58:00 +0530

    stopwatch

--- 15/stopwatch.c
+++ 15/stopwatch.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 void stopwatch() {
-
 }
 
 int main() {
+  printf("type 's' to start\n");
+  printf("type 'p' to pause\n");
+  printf("type 'b' to stop\n");
   stopwatch();
 }
