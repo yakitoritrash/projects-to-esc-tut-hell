a989462c2c4085d258e534d97fccb4c61a2837e0
Arch me <kishudwivedi30@gmail.com>
2025-07-18 21:00:28 +0530

    number game

--- 16/numbergame.c
+++ 16/numbergame.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
-  int upper_bound = 100;
-  int lower_bound= 1;
-  int value = rand() % (upper_bound - lower_bound + 1) + lower_bound;
-  printf(" %d", value);
-  int j = rand();
-  printf("%d", j);
+  srand(time(NULL));
+  int upper_bound = 1;
+  int value = rand() % (upper_bound + 1);
+  int guess;
+  while (guess != value) {
+    printf("Guess the number: \n");
+    scanf("%d\n", &guess);
+    if (guess > value) {
+      printf("too high\n");
+    } else if (guess < value) {
+      printf("too low\n");
+    } else {
+      printf("That's right %d is the number.\n", value);
+    }
+  }
   return 0;
 }
