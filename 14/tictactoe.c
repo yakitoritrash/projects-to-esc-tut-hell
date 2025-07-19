cab8eb516640c6c6c629288ec4e3e15276fdd712
Arch me <kishudwivedi30@gmail.com>
2025-07-19 18:53:21 +0530

    print board

--- 14/tictactoe.c
+++ 14/tictactoe.c
@@ -12,14 +12,17 @@ void tictactoe() {
 }
 void printboard() {
   for (int i = 0; i < 3; i++) {
-    printf("%c | \n", board[i]);
+    printf("%c | ", board[i]);
   }
+  printf("\n");
   for (int i = 3; i < 6; i++) {
-    printf("%c | \n", board[i]);
+    printf("%c | ", board[i]);
   }
+  printf("\n");
   for (int i = 6; i < 9; i++) {
-    printf("%c | \n", board[i]);
+    printf("%c | ", board[i]);
   }
+  printf("\n");
 }
 
 int main() {
