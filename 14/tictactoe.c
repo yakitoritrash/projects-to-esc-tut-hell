1f23207fc8ac1a1da93a8a9856e3ac4ff8815021
Arch me <kishudwivedi30@gmail.com>
2025-07-19 19:13:45 +0530

    printboard

--- 14/tictactoe.c
+++ 14/tictactoe.c
@@ -2,9 +2,9 @@
 
 
 char board[] =
-  {' ', ' ', ' ',
-   ' ', ' ', ' ',
-   ' ', ' ', ' ',};
+  {'X', 'X', 'X',
+   'X', 'X', 'X',
+   'X', 'X', 'X',};
 int size = sizeof(board) / sizeof(board[0]);
 
 void tictactoe() {
@@ -12,19 +12,28 @@ void tictactoe() {
   scanf("%d", &choice);
 }
 void printboard() {
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i <= 2; i++) {
     if (i == 2) {
-      printf("%c ", board[2]);
+      printf("%c", board[2]);
+    } else {
+      printf("%c | ", board[i]);
     }
-    printf("%c | ", board[i]);
   }
-  printf("\n---------------\n");
-  for (int i = 3; i < 6; i++) {
-    printf("%c | ", board[i]);
+  printf("\n---------\n");
+  for (int i = 3; i <= 5; i++) {
+    if (i == 5) {
+      printf("%c", board[5]);
+    } else {
+      printf("%c | ", board[i]);
+    }
   }
-  printf("\n---------------\n");
-  for (int i = 6; i < 9; i++) {
-    printf("%c | ", board[i]);
+  printf("\n---------\n");
+  for (int i = 6; i <= 8; i++) {
+    if (i == 8) {
+      printf("%c", board[8]);
+    } else {
+      printf("%c | ", board[i]);
+    }
   }
   printf("\n");
 }
