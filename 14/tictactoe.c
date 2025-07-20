672aeccc5acc5d3c816733707535d9feb750abd7
Arch me <kishudwivedi30@gmail.com>
2025-07-20 05:47:32 +0530

    winner function

--- 14/tictactoe.c
+++ 14/tictactoe.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void tictactoe(char board[], int size, int player) {
   if (player == 1) {
@@ -31,24 +32,6 @@ void tictactoe(char board[], int size, int player) {
 //    {'0', '1', '2',
 //     '3', '4', '5',
 //     '6', '7', '8',};
-  if (board[0] != ' ' && board[0] == board[1] && board[1] == board[2]
-  || board[0] != ' ' && board[0] == board[3] && board[3] == board[6]
-  || board[0] != ' ' && board[0] == board[4] && board[4] == board[8]) {
-    printf("%c wins.\n", board[0]);
-  }
-  if (board[1] != ' ' && board[1] == board[4] && board[4] == board[7]) {
-    printf("%c wins.\n", board[1]);
-  }
-  if (board[2] != ' ' && board[2] == board[5] && board[5] == board[8]
-  || board[2] != ' ' && board[2] == board[4] && board[4] == board[6]) {
-    printf("%c wins.\n", board[2]);
-  }
-  if (board[3] != ' ' && board[3] == board[4] && board[4] == board[5]) {
-    printf("%c wins.\n", board[3]);
-  }
-  if (board[6] != ' ' && board[6] == board[7] && board[7] == board[8]) {
-    printf("%c wins.\n", board[6]);
-  }
 }
 void printboard(char board[]) {
   for (int i = 0; i <= 2; i++) {
@@ -77,6 +60,33 @@ void printboard(char board[]) {
   printf("\n");
 }
 
+bool winnerfunc(char board[]) {
+  if (board[0] != ' ' && board[0] == board[1] && board[1] == board[2]
+  || board[0] != ' ' && board[0] == board[3] && board[3] == board[6]
+  || board[0] != ' ' && board[0] == board[4] && board[4] == board[8]) {
+    printf("%c wins.\n", board[0]);
+    return true;
+  }
+  if (board[1] != ' ' && board[1] == board[4] && board[4] == board[7]) {
+    printf("%c wins.\n", board[1]);
+    return true;
+  }
+  if (board[2] != ' ' && board[2] == board[5] && board[5] == board[8]
+  || board[2] != ' ' && board[2] == board[4] && board[4] == board[6]) {
+    printf("%c wins.\n", board[2]);
+    return true;
+  }
+  if (board[3] != ' ' && board[3] == board[4] && board[4] == board[5]) {
+    printf("%c wins.\n", board[3]);
+    return true;
+  }
+  if (board[6] != ' ' && board[6] == board[7] && board[7] == board[8]) {
+    printf("%c wins.\n", board[6]);
+    return true;
+  }
+  return false;
+}
+
 int main() {
 char board[] =
   {' ', ' ', ' ',
@@ -84,12 +94,14 @@ char board[] =
    ' ', ' ', ' ',};
 int size = sizeof(board) / sizeof(board[0]);
   for (int i = 0; i <= size - 1; i++) {
-    while (board[i] == ' ') {
+    while (board[i] == ' ' || !winnerfunc(board)) {
       printboard(board);
       tictactoe(board, size, 1);
+      winnerfunc(board);
       printf("\n");
       printboard(board);
       tictactoe(board, size, 2);
+      winnerfunc(board);
       printf("\n");
     }
   }
