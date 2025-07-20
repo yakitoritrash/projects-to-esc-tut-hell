35a7fa3934b416ba0e072125d406706c6e52ce15
Arch me <kishudwivedi30@gmail.com>
2025-07-20 08:11:38 +0530

    word count

--- 17/wordcount.c
+++ 17/wordcount.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 
-
+int main() {
+  FILE *fptr;
+  fptr = fopen("test.txt", "r");
+  int ch;
+  while ((ch = fgetc(fptr)) != EOF) {
+    putchar(ch);
+  }
+  printf("%d", ch);
+  fclose(fptr);
+  return 0;
+}
