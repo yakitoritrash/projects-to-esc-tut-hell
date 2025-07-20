1fccceec4f053e5bfcf2fd83a4c11212e59e9ff1
Arch me <kishudwivedi30@gmail.com>
2025-07-20 09:00:59 +0530

    read stream

--- 17/wordcount.c
+++ 17/wordcount.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main() {
   FILE *fptr;
   fptr = fopen("test.txt", "r");
   int ch;
+  char *word = malloc(10 * sizeof(char));
+  scanf("%s", word);
+  printf("%s", word);
   while ((ch = fgetc(fptr)) != EOF) {
     putchar(ch);
   }
