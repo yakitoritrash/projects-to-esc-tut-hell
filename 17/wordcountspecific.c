76d697a91387dac2ee645889bf817bc93be95483
Arch me <kishudwivedi30@gmail.com>
2025-07-21 23:28:14 +0530

    word specific search

--- 17/wordcountspecific.c
+++ 17/wordcountspecific.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define WORD_BUFFER_SIZE 100
 
 int main() {
   FILE *fptr;
   fptr = fopen("test.txt", "r");
-  int ch;
-  while ((ch = fgetc(fptr)) != EOF) {
-    putchar(ch);
+  if (fptr == NULL) {
+    printf("Error opening file.\n");
+    return 1;
   }
+  int ch;
+  char sword[WORD_BUFFER_SIZE];
+  scanf("%s", sword);
+  printf("%s\n", sword);
+  char word[WORD_BUFFER_SIZE];
+  int count = 0;
+  while (fscanf(fptr, "%s", word) != EOF) {
+    if (strcmp(word, sword) == 0) {
+      count++;
+    }
+  }
+  printf("%d", count);
   return 0;
 }
