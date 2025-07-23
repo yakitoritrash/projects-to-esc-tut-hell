be8674cf1e36b455c510f85e9b499f552751b887
Arch me <kishudwivedi30@gmail.com>
2025-07-24 01:37:26 +0530

    url shortener

--- 18/urlshortener.c
+++ 18/urlshortener.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
-void hashFunction() {
+#define MAX_URL_LENGTH 2048
+#define SHORT_URL_LENGTH 6
 
+void hashFunction(char *longURL, char *shortURL) {
+  unsigned long hash = 5381;
+  int c;
+
+  while ((c = *longURL++)) {
+    hash = ((hash << 5) + hash) + c;
+  }
+
+  snprintf(shortURL, SHORT_URL_LENGTH, "%lx", hash % 1000000);
 }
+
+
