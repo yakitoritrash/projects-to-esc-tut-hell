79c7d9b3fbcb34fdf687e444a1cedde80f890af0
Arch me <kishudwivedi30@gmail.com>
2025-07-29 18:12:59 +0530

    todo

--- 19/todo.c
+++ 19/todo.c
@@ -4,9 +4,14 @@
 void todo() {
   char choice;
   scanf("%c", &choice);
+  
   if (choice == 'a') {
 
   }
 }
 int main() {
+  printf("Type 'a' to add a task.\n");
+  printf("Type 'd' to delete a task.\n");
+  printf("Type 'v' to view the task list.\n");
+  todo();
 }
