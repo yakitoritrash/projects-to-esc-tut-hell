3890b716e5c1928ebc62b1a064658b8eb488faf0
Arch me <kishudwivedi30@gmail.com>
2025-08-15 00:21:11 +0530

    to do list

--- 19/todo.c
+++ 19/todo.c
@@ -38,5 +38,41 @@ task_t *delete_task(int pos, task_t *head) {
 }
 
 void printlist(task_t *head) {
+  task_t *tmp = head;
+  int i = 0;
+  while (tmp != NULL) {
+    printf("%d: %99s\n", i, tmp->task);
+    tmp = tmp->next;
+    i++;
+  }
+}
 
+int main() {
+  task_t *head = NULL;
+  printf("Type 'A' to add a task, 'R' to delete, 'P' to show the list, 'E' to exit.\n");
+  while (1) {
+    char x;
+    scanf("%c", &x);
+    if (x == 'A') {
+      char *y;
+      scanf("%99s", y);
+      head = insert_task(y, head);
+      printf("task added.\n");
+    } else if (x == 'R') {
+      printlist(head);
+      printf("Choose the task to delete\n");
+      int y;
+      scanf("%d", &y);
+      head = delete_task(y, head);
+      printf("task deleted.\n");
+    } else if (x == 'P') {
+      printlist(head);
+      printf("\n");
+    } else if (x == 'E') {
+      break;
+    } else {
+      printf("choose some choice from the list.\n");
+    }
+  }
+  return 0;
 }
