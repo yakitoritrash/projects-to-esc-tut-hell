e7091189ee18a3a77712872fc386f874062605f1
Arch me <kishudwivedi30@gmail.com>
2025-08-15 00:02:38 +0530

    to do through linked list?

--- 19/todo.c
+++ 19/todo.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
+typedef struct task_t {
+  char *task; 
+  struct task_t *next;
+} task_t;
+
+task_t *create_task(char *task) {
+  task_t *new_task = malloc(sizeof(task_t));
+  if (!new_task) {
+    return NULL;
+  }
+  new_task->task = task;
+  new_task->next = NULL;
+  return new_task;
+}
+
+task_t *insert_task(char *task, 
