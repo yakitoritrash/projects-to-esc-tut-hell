a6b59e5d94872df915198312d1b63c63fc9fbd4d
Arch me <kishudwivedi30@gmail.com>
2025-08-14 23:53:19 +0530

    to do list

--- 19/todo.c
+++ 19/todo.c
@@ -2,19 +2,3 @@
 #include <string.h>
 #include <stdlib.h>
 
-void todo() {
-  char choice;
-  scanf("%c", &choice);
-  char **strings = NULL;
-  int size = 5;
-  strings = (char**)malloc(sizeof(char*));
-  if (choice == 'a') {
-
-  }
-}
-int main() {
-  printf("Type 'a' to add a task.\n");
-  printf("Type 'd' to delete a task.\n");
-  printf("Type 'v' to view the task list.\n");
-  todo();
-}
