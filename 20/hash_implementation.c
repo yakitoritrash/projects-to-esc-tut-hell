9af54f01e72fcaca0e784bf934021c225e988d50
Arch me <kishudwivedi30@gmail.com>
2025-08-18 11:39:18 +0530

    tokenization

--- 20/hash_implementation.c
+++ 20/hash_implementation.c
@@ -4,9 +4,18 @@
 
 int main(void) {
   const char *file_path = "t8.shakespeare.txt";
-  Nob_String_Builder content = {0};
-  if (!nob_read_entire_file(file_path, &content)) return 1;
+  Nob_String_Builder buf = {0};
+  if (!nob_read_entire_file(file_path, &buf)) return 1;
+  nob_log(NOB_INFO, "Size of %s is %zu bytes", file_path, buf.count);
 
-  nob_log(NOB_INFO, "Size of %s is %zu bytes", file_path, content.count);
+  Nob_String_View content = {
+    .data = buf.items,
+    .count = buf.count,
+  };
+
+  for (int i = 0; i < 100 && content.count > 0; i++) {
+    Nob_String_View token = nob_sv_chop_by_delim(&content, ' ');
+    nob_log(NOB_INFO, " "SV_Fmt, SV_Arg(token));
+  }
   return 0;
 }
