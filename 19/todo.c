8b757e17674d3a8935f7df2393a73e589aa9d5bd
Arch me <kishudwivedi30@gmail.com>
2025-08-18 11:12:52 +0530

    todo

--- 19/todo.c
+++ 19/todo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct task_t {
   char *task; 
@@ -11,7 +12,8 @@ task_t *create_task(char *task) {
   if (!new_task) {
     return NULL;
   }
-  new_task->task = task;
+  new_task->task = malloc(strlen(task) + 1);
+  strcpy(new_task->task, task);
   new_task->next = NULL;
   return new_task;
 }
@@ -27,17 +29,40 @@ task_t *insert_task(char *task, task_t *head) {
 }
 
 task_t *delete_task(int pos, task_t *head) {
+  if (head == NULL) {
+    return NULL;
+  }
   task_t *tmp = head;
-  for (int i = 0; i < pos; i++) {
-    tmp = tmp->next;
+  if (pos == 0) {
+    task_t *to_delete = head;
+    if (head->next != NULL) {
+      head = head->next;
+    }
+    free(to_delete->task);
+    free(to_delete);
+    return head;
+  }
+  for (int i = 0; i < pos - 1; i++) {
+    if (tmp->next != NULL) {
+      tmp = tmp->next;
+    }
+  }
+  if (tmp == NULL || tmp->next == NULL) {
+    printf("Invalid position\n");
+    return head;
   }
   task_t *to_delete = tmp->next;
   tmp->next = to_delete->next;
+  free(to_delete->task);
   free(to_delete);
   return head;
 }
 
 void printlist(task_t *head) {
+  if (head == NULL) {
+    printf("NULL\n");
+    return;
+  }
   task_t *tmp = head;
   int i = 0;
   while (tmp != NULL) {
@@ -47,15 +72,23 @@ void printlist(task_t *head) {
   }
 }
 
+void free_list(task_t *head) {
+  while (head) {
+    task_t *tmp = head;
+    head = head->next;
+    free(tmp->task);
+    free(tmp);
+  }
+}
 int main() {
   task_t *head = NULL;
   printf("Type 'A' to add a task, 'R' to delete, 'P' to show the list, 'E' to exit.\n");
   while (1) {
     char x;
-    scanf("%c", &x);
+    scanf(" %c", &x);
     if (x == 'A') {
-      char *y;
-      scanf("%99s", y);
+      char y[100];
+      scanf(" %99s[^\n]", y);
       head = insert_task(y, head);
       printf("task added.\n");
     } else if (x == 'R') {
@@ -74,5 +107,6 @@ int main() {
       printf("choose some choice from the list.\n");
     }
   }
+  free_list(head);
   return 0;
 }
