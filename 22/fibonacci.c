6f301e61bbad9666953acdbc1def3ecc01abc3d3
Arch me <kishudwivedi30@gmail.com>
2025-09-06 18:24:08 +0530

    fib

---
+++ 22/fibonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  int arr[n];
+  arr[0] = 0;
+  arr[1] = 1;
+  for (int i = 2; i < n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+}
